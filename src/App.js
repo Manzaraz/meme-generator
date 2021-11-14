cdb29819af8262925a20a0dfca4dba733cf523af
Christian Manzaraz <christianmanzaraz@MacBook-Air-de-Christian.local>
2021-11-14 18:45:12 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import { useState } from "react";
+import html2canvas from "html2canvas";
 
 function App() {
+  const [linea1, setLinea1] = useState("");
+  const [linea2, setLinea2] = useState("");
+  const [image, setImage] = useState("");
+
+  const handleChange1 = function (e) {
+    setLinea1(e.target.value);
+  };
+  const handleChange2 = function (e) {
+    setLinea2(e.target.value);
+  };
+  const handleSelect = function (e) {
+    setImage(e.target.value);
+  };
+  const handleClick = function (e) {
+    html2canvas(document.querySelector("#meme")).then((canvas) => {
+      var img = canvas.toDataURL("image/jpg");
+
+      var link = document.createElement("a");
+      link.download = "meme.jpg";
+      link.href = img;
+      link.click();
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <select onChange={handleSelect}>
+        <option value="" selected disabled hidden>
+          Elige un meme
+        </option>
+        <option value="fire">Casa en Llamas</option>
+        <option value="futurama">Futurama</option>
+        <option value="history">History Channel</option>
+        <option value="matrix">Matrix</option>
+        <option value="philosoraptor">Philosoraptor</option>
+        <option value="smart">Smart Guy</option>
+      </select>
+      <br />
+      <input
+        onChange={handleChange1}
+        type="text"
+        name=""
+        placeholder="linea 1"
+      />{" "}
+      <br />
+      <input
+        onChange={handleChange2}
+        type="text"
+        name=""
+        placeholder="linea 2"
+      />{" "}
+      <br />
+      <br />
+      <button onClick={handleClick}>Exportar</button>
+      <div className="meme" id="meme">
+        <span>{linea1}</span>
+        <span>{linea2}</span>
+        {image && <img src={`./img/${image}.jpg`} alt="custom meme" />}
+      </div>
     </div>
   );
 }
